525f06b13809616efddda55a6ee28e3ee43ed4cb
Harshit Srivastav <harshitsrivastav874@gmail.com>
2026-08-01 14:24:19 +0530

    change

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -9,7 +9,7 @@ const Post = ({ post }) => {
             </h5>
             <p className="card-text">{post.body}</p>
             {post.tags.map((tag) => (
-                <span class="badge text-bg-primary hashtag">{tag}</span>
+                // <span class="badge text-bg-primary hashtag">{tag}</span>
             ))}
         </div>
     </div>;
